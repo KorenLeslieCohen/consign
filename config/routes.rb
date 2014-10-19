dc26641c18014bb8f84986cd5842d3905361060c
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-10-18 22:06:05 -0400

    created user index and show route and views

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
+  resources :users, only: [:index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
