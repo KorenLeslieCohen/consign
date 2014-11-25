23baa9fa788edb3874c7d30f8207920c2d5ebe31
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-11-25 07:04:47 -0500

    added limited seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+chanel = Designer.create(:name => "Chanel")
+hermes = Designer.create(:name => "Hermes")
+
+highend = Biztag.create(:name => "High End")
+boutique = Biztag.create(:name => "Boutique")
+
+chanel_photo = Tag.create(:name => "Chanel")
+hermes_photo = Tag.create(:name => "Hermes")
+
+manhattan = Borough.create(:name => "Manhattan")
+bronx = Borough.create(:name => "The Bronx")
+brooklyn = Borough.create(:name => "Brooklyn")
+staten_island = Borough.create(:name => "Staten Island")
+jersey = Borough.create(:name => "New Jersey")
+queens = Borough.create(:name => "Queens")
+
+ues = Neighborhood.create(:name => "Upper East Side")
+williamsburg = Neighborhood.create(:name => "Williamsburg")
