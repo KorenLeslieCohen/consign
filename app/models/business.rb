65fee18c0415d10ea841b4256a310e3bfa19007d
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-12-05 21:41:45 -0500

    added profile photo to business

--- app/models/business.rb
+++ app/models/business.rb
@@ -12,4 +12,20 @@ class Business < ActiveRecord::Base
   has_many :designers, through: :business_designers
   accepts_nested_attributes_for :designers
 
+  has_attached_file :profile_photo, 
+    :storage => :s3, 
+    # :hash_secret => "longSecretString", 
+    # :path => ":hash", 
+    :bucket => "consignnyc",
+    :s3_credentials => "#{Rails.root}/config/aws.yml",
+    # :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
+    # :s3_credentials => S3_CREDENTIALS,
+    # :styles => { :thumb => "1024x768"}, 
+    # :default_style => :thumb, 
+    :s3_protocol => "https"
+
+  # validates :profile_photo, :attachment_presence => true
+  validates_with AttachmentSizeValidator, :attributes => :profile_photo, :less_than => 3.megabytes
+  validates_attachment_content_type :profile_photo, :content_type => /\Aimage\/.*\Z/
+ 
 end
