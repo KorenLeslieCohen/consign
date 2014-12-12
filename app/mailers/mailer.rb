09cacf8a667aafee26ab6d7f37080c3d88519ee1
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-12-12 01:52:43 -0500

    comments on mailer.rb

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,30 +1,37 @@
 class Mailer < ActionMailer::Base
+
+  # default email
   default from: "ConsignDotNYC@gmail.com"
 
+  # sends user confirmation email following review
   def new_review_email(user, business)
     @user = user
     @business = business
     mail(:to => @user.email, :subject => "Your review has been posted on Consign.NYC")
   end
 
+  # sends me email following user review
   def admin_review_email(user, business)
     @user = user
     @business = business
     mail(:to => "consigndotnyc@gmail.com", :subject => "New Review on Consign.NYC")
-   end
+  end
 
+  # sends user confirmation email following photo
   def new_photo_email(user, business)
     @user = user
     @business = business
     mail(:to => @user.email, :subject => "Your photo has been uploaded on Consign.NYC")
-   end
+  end
 
+  # sends me email following user review
   def admin_photo_email(user, business)
     @user = user
     @business = business
     mail(:to => "consigndotnyc@gmail.com", :subject => "New Photo on Consign.NYC")
   end
 
+  # contact form
   def message_email(name, email, body)
     @name = name
     @email = email
