a1c0975c63b5b7243fde61393602c025aeb41ba7
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-12-13 01:32:00 -0500

    added bio migration to users and associated code

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141212050033) do
+ActiveRecord::Schema.define(version: 20141213062857) do
 
   create_table "biztags", force: true do |t|
     t.string   "name"
@@ -138,6 +138,7 @@ ActiveRecord::Schema.define(version: 20141212050033) do
     t.datetime "user_profile_photo_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "bio"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
