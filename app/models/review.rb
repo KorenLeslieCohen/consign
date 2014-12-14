99428bb1596cdd1955b8642977ed0bcd3ae46148
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-12-14 02:10:28 -0500

    working on errors for nested attributes; dates on reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,10 +4,10 @@ class Review < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :business_id, :user_id
-  # validates :rating, presence: true, inclusion: {in: 1..5,
-                                                #  message: "Rating must be between 1 and 5 stars."
-                                                # }
-  # validates :content, presence: true, length: {minimum: 10}
+  validates :rating, presence: true, inclusion: {in: 1..5,
+                                                 message: "Rating must be between 1 and 5."
+                                                }
+  validates :content, presence: true, length: {minimum: 30}
 
   # Action Mailer
   after_create :send_review_email
