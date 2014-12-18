beb3d7871eb64b8683504be1c7737525d2dd2fa2
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-12-18 07:12:07 -0500

    added error messages with remote true to message form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -30,10 +30,11 @@ class MessagesController < ApplicationController
     respond_to do |format|
       if @message.save
         format.html { redirect_to @message, notice: 'Message was successfully created.' }
-        format.js {}
+        format.js   { }
         format.json { render :show, status: :created, location: @message }
       else
         format.html { render :new }
+        format.js   { render 'error' }
         format.json { render json: @message.errors, status: :unprocessable_entity }
       end
     end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,9 @@
 class Message < ActiveRecord::Base
 
+  validates :name, presence: true 
+  validates :email, presence: true # use an email validation
+  validates :body, presence: true
+
   after_create :send_message_email
 
   def send_message_email
