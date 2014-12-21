42f49cb677ac71c540fe9391b0be46ef0a666fff
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-12-21 18:22:49 -0500

    started for stores page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -28,7 +28,7 @@ class PhotosController < ApplicationController
 
     respond_to do |format|
       if @photo.save
-        format.html { redirect_to @photo.business, notice: 'Photo was successfully created.' }
+        format.html { redirect_to @photo.business, notice: 'Photo was successfully uploaded.' }
         format.json { render :show, status: :created, location: @photo }
       else
         format.html { redirect_to @photo.business, notice: 'Please make sure description is less than 20 characters and photo is less than 3 mb.' }
