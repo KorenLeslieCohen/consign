14c169999f6e763f5fbdb44c0361498eb66b8b7f
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-12-25 23:05:19 -0500

    hours modal

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,17 +1,3 @@
-// $(document).ready(function() {
- 
-//   $("#owl-demo").owlCarousel({
- 
-//       autoPlay: 3000, //Set AutoPlay to 3 seconds
- 
-//       items : 4
-//       // itemsDesktop : [1199,3],
-//       // itemsDesktopSmall : [979,3]
- 
-//   });
- 
-// });
-
 $(document).ready(function() {
  
   var owl = $("#owl-demo");
@@ -19,19 +5,8 @@ $(document).ready(function() {
   owl.owlCarousel({
 
     items : 3,
-     
-      // itemsCustom : [
-      //   [0, 2],
-      //   [450, 4],
-      //   [600, 7],
-      //   [700, 9],
-      //   [1000, 10],
-      //   [1200, 12],
-      //   [1400, 13],
-      //   [1600, 15]
-      // ],
-      navigation : true,
-      pagination: false
+    navigation : true,
+    pagination: false
  
   });
  
--- app/models/business.rb
+++ app/models/business.rb
@@ -24,6 +24,7 @@ class Business < ActiveRecord::Base
     # :default_style => :thumb, 
     :s3_protocol => "https"
 
+  validates_presence_of :name, :street_address, :city, :state, :zip, :phone
   validates :admin_email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :profile_photo, :attachment_presence => true
