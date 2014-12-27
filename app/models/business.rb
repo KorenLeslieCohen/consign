11a5af5149e32ccdabadc6ae67c5c38d92918633
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-12-26 19:21:04 -0500

    sorted lists of shops and designers alphabetically, and random styling

--- app/models/business.rb
+++ app/models/business.rb
@@ -24,10 +24,10 @@ class Business < ActiveRecord::Base
     # :default_style => :thumb, 
     :s3_protocol => "https"
 
-  validates_presence_of :name, :street_address, :city, :state, :zip, :phone
+  validates_presence_of :name, :street_address, :city, :state, :zip, :phone, :map, :percent_to_client, :consignment_period, :return_policy
   validates :admin_email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-  validates :profile_photo, :attachment_presence => true
+  # validates :profile_photo, :attachment_presence => true
   validates_with AttachmentSizeValidator, :attributes => :profile_photo, :less_than => 3.megabytes
   validates_attachment_content_type :profile_photo, :content_type => /\Aimage\/.*\Z/
  
