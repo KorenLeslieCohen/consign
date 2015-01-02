d7320cdbea3b87447be1c2e4d2894e34309f8486
Koren Leslie Cohen <koren.cohen@gmail.com>
2015-01-01 22:48:07 -0500

    admin tables

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:show, :edit, :update] # probably want to keep using this
+  before_action :set_user, only: [:show, :edit, :update]
 
   # GET /users
   # GET /users.json
