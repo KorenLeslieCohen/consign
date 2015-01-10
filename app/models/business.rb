7947bb000cc29db77bc21a56d8dabbed8262d01f
Koren Leslie Cohen <koren.cohen@gmail.com>
2015-01-10 02:16:26 -0500

    modified email format validation to allow for blank entry

--- app/models/business.rb
+++ app/models/business.rb
@@ -22,8 +22,8 @@ class Business < ActiveRecord::Base
   # Validations
   validates :store_bio, length: {maximum: 700}
   validates_presence_of :name, :street_address, :city, :state, :zip, :phone, :map, :percent_to_client, :consignment_period, :return_policy
-  # validates :admin_email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :admin_email, :allow_blank => true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :email, :allow_blank => true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   # validates :profile_photo, :attachment_presence => true
   validates_with AttachmentSizeValidator, :attributes => :profile_photo, :less_than => 3.megabytes
   validates_attachment_content_type :profile_photo, :content_type => /\Aimage\/.*\Z/
