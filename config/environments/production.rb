204434678835b768de866bfa9e3bf4993a5fe5d4
Koren Leslie Cohen <koren.cohen@gmail.com>
2015-01-16 23:22:23 -0500

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   # config.assets.precompile = ['*.js', '*.css', '*.css.erb']
 
   # Action Mailer / Devise Mailers 
-  config.action_mailer.default_url_options =   { :host => 'consign.nyc' }
+  # config.action_mailer.default_url_options =   { :host => 'consign.nyc' }
   # config.action_mailer.delivery_method = :smtp
   # ActionMailer::Base.smtp_settings = {
   #   :address        => 'smtp.sendgrid.net',
@@ -96,6 +96,17 @@ Rails.application.configure do
   #   :domain         => 'heroku.com',
   #   :enable_starttls_auto => true
   # }
+
+  config.action_mailer.default_url_options = { :host => 'consign.nyc' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
+
   # config.action_mailer.smtp_settings = {
   #       address: "smtp.gmail.com",
   #       port: "587",
