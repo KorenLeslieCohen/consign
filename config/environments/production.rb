15ba20459cfe17d0b3746a5beaec9cc49454a53e
Koren Leslie Cohen <koren.cohen@gmail.com>
2015-01-16 23:47:50 -0500

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 
   # Action Mailer / Devise Mailers 
   config.action_mailer.default_url_options =   { :host => 'consign.nyc' }
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
   # ActionMailer::Base.smtp_settings = {
   #   :address        => 'smtp.sendgrid.net',
   #   :port           => '587',
@@ -98,24 +98,24 @@ Rails.application.configure do
   # }
 
   # config.action_mailer.default_url_options = { :host => 'consign.nyc' }
-  # ActionMailer::Base.smtp_settings = {
-  #   :address        => "smtp.sendgrid.net",
-  #   :port           => "25",
-  #   :authentication => :plain,
-  #   :user_name      => ENV['SENDGRID_USERNAME'],
-  #   :password       => ENV['SENDGRID_PASSWORD'],
-  #   :domain         => ENV['SENDGRID_DOMAIN']
-  # }
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
 
-  config.action_mailer.smtp_settings = {
-        address: "smtp.sendgrid.net",
-        port: "587",
-        domain: "heroku.com",
-        authentication: "plain",
-        enable_starttls_auto: true,
-        user_name: ENV['ENDGRID_USERNAME'],
-        password: ENV['SENDGRID_PASSWORD']
-    }
+  # config.action_mailer.smtp_settings = {
+  #       address: "smtp.sendgrid.net",
+  #       port: "587",
+  #       domain: "heroku.com",
+  #       authentication: "plain",
+  #       enable_starttls_auto: true,
+  #       user_name: ENV['ENDGRID_USERNAME'],
+  #       password: ENV['SENDGRID_PASSWORD']
+  #   }
 
   # config.action_mailer.smtp_settings = {
   #       address: "smtp.gmail.com",
@@ -135,8 +135,8 @@ Rails.application.configure do
   #   location: '/usr/sbin/sendmail',
   #   arguments: '-i -t'
   # }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.default_options = {from: 'no-reply@consign.nyc'} 
 
   # Amazon Web Services S3
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,8 +10,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  # config.mailer_sender = 'no-reply@consign.nyc'
-  config.mailer_sender = "mail-to-send@from.com"
+  config.mailer_sender = 'no-reply@consign.nyc'
+  # config.mailer_sender = "mail-to-send@from.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
