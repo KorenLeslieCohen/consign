6f00dbfe725c1e9c43739690ddaa398adc50c872
Koren Leslie Cohen <koren.cohen@gmail.com>
2015-01-17 00:09:54 -0500

    sendgrid working, deleted commented code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,18 +86,6 @@ Rails.application.configure do
 
   # Action Mailer / Devise Mailers 
   config.action_mailer.default_url_options =   { :host => 'consign.nyc' }
-  # config.action_mailer.delivery_method = :smtp
-  # ActionMailer::Base.smtp_settings = {
-  #   :address        => 'smtp.sendgrid.net',
-  #   :port           => '587',
-  #   :authentication => :plain,
-  #   :user_name      => ENV['SENDGRID_USERNAME'],
-  #   :password       => ENV['SENDGRID_PASSWORD'],
-  #   :domain         => 'heroku.com',
-  #   :enable_starttls_auto => true
-  # }
-
-  # config.action_mailer.default_url_options = { :host => 'consign.nyc' }
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.sendgrid.net",
     :port           => "25",
@@ -106,38 +94,6 @@ Rails.application.configure do
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => ENV['SENDGRID_DOMAIN']
   }
-
-  # config.action_mailer.smtp_settings = {
-  #       address: "smtp.sendgrid.net",
-  #       port: "587",
-  #       domain: "heroku.com",
-  #       authentication: "plain",
-  #       enable_starttls_auto: true,
-  #       user_name: ENV['ENDGRID_USERNAME'],
-  #       password: ENV['SENDGRID_PASSWORD']
-  #   }
-
-  # config.action_mailer.smtp_settings = {
-  #       address: "smtp.gmail.com",
-  #       port: "587",
-  #       domain: "gmail.com",
-  #       authentication: "plain",
-  #       enable_starttls_auto: true,
-  #       user_name: ENV['GMAIL_EMAIL'],
-  #       password: ENV['GMAIL_PASSWORD']
-  #   }
-  # config.action_mailer.smtp_settings = {:address => "no-reply@consign.nyc"}
-  # config.mailer_sender = 'no-reply@consign.nyc'
-
-  # config.action_mailer.delivery_method = :sendmail
-  # Defaults to:
-  # config.action_mailer.sendmail_settings = {
-  #   location: '/usr/sbin/sendmail',
-  #   arguments: '-i -t'
-  # }
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.default_options = {from: 'no-reply@consign.nyc'} 
 
   # Amazon Web Services S3
   config.paperclip_defaults = {
